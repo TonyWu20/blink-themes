7fdd7f5e106d988fae8669fa041c639202c3fb17
TonyWu20 <tony.w21@gmail.com>
2022-06-09 23:27:25 +0800

    First commit of adaptation of catpuccin macchiato

---
+++ themes/catppuccin-macchiato.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#24273a", "#ed8796", "#a6da95", "#eed49f", "#8aadf4", "#c6a0f6", "#8bd5ca", "#f4dbd6", "#5b6078", "#ee99a0", "#f5a97f", "#7dc4e4", "#f0c6c6", "#8ec07c", "#f4dbd6"]);
+t.prefs_.set('foreground-color', "#cad3f5");
+t.prefs_.set('background-color', "#363a4f");
+t.prefs_.set('cursor-color', 'rgba(235,219,178,0.5)');
